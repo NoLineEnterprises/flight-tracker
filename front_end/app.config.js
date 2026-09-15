291155d9342f3962418071f92df9c7ed78018635
NoLineEnterprises <scott.e.bryant@gmail.com>
2026-09-15 18:50:52 -0500

    Move Google Maps key to environment config

---
+++ front_end/app.config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  expo: {
+    name: "flight-track",
+    slug: "android",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "android",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true
+    },
+
+    android: {
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      },
+
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.flighttrack.app"
+    },
+
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000"
+          }
+        }
+      ]
+    ],
+
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+
+    extra: {
+      router: {},
+      eas: {
+        projectId: "5336d4ad-dbe7-4cdd-9754-d79b528b58a5"
+      }
+    }
+  }
+};
